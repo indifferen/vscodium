0d7bc12c9ac2e43e4ad42757da6a704c17b756b0
juan <xuhuanvtt@gmail.com>
2025-07-17 15:01:01 +0800

    22230

--- get_repo.sh
+++ get_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-RELEASE_VERSION=1.70.2
+RELEASE_VERSION=1.70.2.22230
 # git workaround
 if [[ "${CI_BUILD}" != "no" ]]; then
   git config --global --add safe.directory /__w/vscodium/vscodium
